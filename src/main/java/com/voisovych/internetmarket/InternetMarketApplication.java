d800ffbfbbbeff12cdde307bfd6636e7a4518378
v-voisivych <voys1441@gmail.com>
2020-10-22 17:42:11 +0300

    .

---
+++ src/main/java/com/voisovych/internetmarket/InternetMarketApplication.java
@@ -0,0 +1,13 @@
+package com.voisovych.internetmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetMarketApplication.class, args);
+    }
+
+}
